208091e599065d82dec5fa1df831366db356421c
nakayamalee <93628098+nakayamalee@users.noreply.github.com>
2023-12-12 17:29:58 +0800

    test

---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  mounted() {
+    fetch('http://localhost:3000/660/posts', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${sessionStorage.getItem('accessToken')}`,
+        },
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+        })
+  },
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/LoginAccount.vue
@@ -0,0 +1,59 @@
+<script>
+
+export default {
+  data() {
+    return {
+      userData: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    submitData() {
+      fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...this.userData }),
+      })
+        .then(res => res.json())
+        .then(data => {
+          sessionStorage.setItem('accessToken', data.accessToken);
+        })
+    },
+  },
+};
+</script>
+
+<template>
+  <section id="register">
+    <form class="flex flex-col gap-3" @submit.prevent="submitData()">
+      <label>
+        帳號:
+        <input v-model="userData.email" type="email" name="email" required>
+      </label>
+      <label>
+        密碼:
+        <input v-model="userData.password" type="password" name="password" required>
+      </label>
+      <div class="btns">
+        <button type="submit">登入</button>
+        <button type="">返回</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+#register {
+  .btns {
+    @apply flex gap-3;
+  }
+  input {
+    @apply text-black;
+  }
+}
+</style>
